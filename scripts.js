da74985f9bfb7f6d323c8792533ff8307989f0a9
Trent Oswald <trentoswald@therebelrobot.com>
2014-08-15 09:25:25 -0600

    Corrected indents and added structure notes

--- scripts.js
+++ scripts.js
@@ -1,23 +1,31 @@
 $(document).ready(function(){
 
+  /*** Event listeners (on keypress, on click, etc.) ***/
+  $("input").keypress(function(event) {
+  	if (event.which == 13) {
+  		event.preventDefault();
+    	_afterEnterPress();	
+    }
+  });
 
+  // Place here any additional event listeners (Remember, SELECTOR.EVENT(function(){}))
 
-$("input").keypress(function(event) {
-	if (event.which == 13) {
-		event.preventDefault();
-	_afterEnterPress();	
-}
-});
+  /*** END Event Listeners (Do not place any beyond this point) ***/
+  /*** Internal Functions (Functions to run when listener is triggered) ***/
 
-function _afterEnterPress (){
-//This is retrieving the input
-var newtodoitem = $("input").val();
-//This is clearing the input
-$("input").val("");
-
-
-
-}
+  function _afterEnterPress (){
+    //This is retrieving the input
+    var newtodoitem = $("input").val();
+    //This is clearing the input
+    $("input").val("");
+    
+    // Push the info to the ul
+  }
+  
+  // Place here any additional internal functions (with underscored titles, _afterLiClick for example)
+  
+  /*** END Internal Functions (do not put any code beyond this point) ***/
+  
 })
 
 
