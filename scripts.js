aaf1ea5c58e3586b76faba7c3b73f82d15a54e8b
Daren Hillhouse <sandduner350@gmail.com>
2014-08-14 21:28:47 -0600

    added some code to put in the afterEnterPress, the camel case!!!

--- scripts.js
+++ scripts.js
@@ -1,10 +1,28 @@
+$(document).ready(function(){
+
 
 
 $("input").keypress(function(event) {
 	if (event.which == 13) {
 		event.preventDefault();
-		$("form").submit();
+	_afterEnterPress();	
 }
 });
+
+function _afterEnterPress (){
+//This is retrieving the input
+var newtodoitem = $("input").val();
+//This is clearing the input
+$("input").val("");
+
+
+
+}
+})
+
+
+
+
+
 
 
